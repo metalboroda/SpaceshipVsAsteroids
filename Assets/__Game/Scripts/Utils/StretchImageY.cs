ce855ad3948fb6aae1ac4100309402a2dbd59d8d
dageron93 <vladb93games@gmail.com>
2024-04-04 17:52:56 +0300

    First

---
+++ Assets/__Game/Scripts/Utils/StretchImageY.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class StretchImageY : MonoBehaviour
+{
+  private RectTransform _imageTransform;
+
+  void Awake()
+  {
+    _imageTransform = GetComponent<RectTransform>();
+  }
+
+  void Update()
+  {
+    Vector2 imageSize = _imageTransform.sizeDelta;
+    Vector2 screenSize = new Vector2(Screen.width, Screen.height);
+    float aspectRatio = imageSize.x / imageSize.y;
+
+    imageSize.y = screenSize.y;
+    imageSize.x = imageSize.y * aspectRatio;
+
+    _imageTransform.sizeDelta = imageSize;
+  }
+}
